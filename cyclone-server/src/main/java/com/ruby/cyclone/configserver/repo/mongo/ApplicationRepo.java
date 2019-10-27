75310cb52239d00d0b1b4b085e870c6bc6558070
cimpapps <catalincimpoeru24@gmail.com>
2019-10-27 22:23:28 +0200

    refactor a lot

--- cyclone-server/src/main/java/com/ruby/cyclone/configserver/repo/mongo/ApplicationRepo.java
+++ cyclone-server/src/main/java/com/ruby/cyclone/configserver/repo/mongo/ApplicationRepo.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 public interface ApplicationRepo extends MongoRepository<Application, AppId> {
 
-    @Query("{'namespace.tenant': ?0, 'namespace.namespace': ?1}")
+    @Query("{'_id.namespace.tenant': ?0, '_id.namespace.namespace': ?1}")
     Set<Application> findAllByNamespace(String tenant, String namespace);
 
 }
