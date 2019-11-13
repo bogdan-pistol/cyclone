0a1f305dc99cd73b8d6b3143179010592fe122b8
Catalin.Cimpoeru <catalin.cimpoeru@orange.com>
2019-11-13 18:11:43 +0100

    add ionic

--- cyclone-server/src/test/java/com/ruby/cyclone/configserver/repo/mongo/PropertiesRepoTest.java
+++ cyclone-server/src/test/java/com/ruby/cyclone/configserver/repo/mongo/PropertiesRepoTest.java
@@ -1,11 +1,16 @@
 package com.ruby.cyclone.configserver.repo.mongo;
 
 
+import com.ruby.cyclone.configserver.models.business.Property;
+import com.ruby.cyclone.configserver.models.business.PropertyId;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @DataMongoTest
@@ -18,14 +23,18 @@ public class PropertiesRepoTest {
     @Test
     public void save() {
 
-//        Property<String> property = new Property<>();
-//
-//        String key = "user.name";
-//        property.setKey(key);
-//        property.setValue("ruby");
-//        propertiesRepo.save(property);
-//        Optional<Property> byId = propertiesRepo.findById(key);
-//        Assert.assertEquals(property, byId.get());
+        Property<String> property = new Property<>();
+
+        PropertyId id = PropertyId.builder().namespace("ns")
+                .key("key")
+                .file("file").build();
+        String key = "user.name";
+        property.setValue(key);
+        property.setId(id);
+        property.setValue("ruby");
+        propertiesRepo.save(property);
+        Optional<Property> byId = propertiesRepo.findById(id);
+        Assert.assertEquals(property, byId.get());
 
     }
 }
